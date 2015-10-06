1f02fa162d2772c0d353c3b33c7d96087b695257
ao-dexter <dexter@athleticoffice.com>
2015-10-06 18:48:49 -0500

    Delete metadata.rb

--- portal/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             "portal"
-description      'Configures portal layer'
-long_description 'Custom Portal Layer'
-maintainer       "AO"
-license          "Apache 2.0"
-version          "1.0.0"
-
-depends 'opsworks_commons'
